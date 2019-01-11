744b5bd4902b5c91bea559cb0f578c4218b24d03
Sihyeon Lee <sihyeon.dev@gmail.com>
2019-01-11 13:51:26 +0900

    feat : Add call_instance
    배포용 call_instance.py 추가

---
+++ ec2-control/call_instance.py
@@ -0,0 +1,12 @@
+def return_instance():
+    instance_id = ''
+
+    return instance_id
+
+
+def return_client():
+    service = ''
+    location = ''
+    client_info = [service, location]
+
+    return client_info
